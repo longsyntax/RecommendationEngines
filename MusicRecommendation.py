a3c833bee11ba7f0212690d5e352c6afdcb655dc
Abhay Bhat <longsyntax@gmail.com>
2017-01-04 18:46:18 +0530

    Update and rename music.py to MusicRecommendation.py

--- MusicRecommendation.py
+++ MusicRecommendation.py
@@ -1,23 +1,23 @@
 import pandas as pd
 from scipy.spatial.distance import cosine
  
-fullData = pd.read_csv('C:/Users/Abhay Bhat/Downloads/lastfm-matrix-germany.csv')
+fullData = pd.read_csv('musicdata.csv')
 
 musicData = fullData.drop('user', 1)
  
-# Creating artist vs astist matrix
+# Creating artist vs astist matrix with blanks
 itemBased = pd.DataFrame(index = musicData.columns, columns = musicData.columns)
 
-# Filling values to the matrix with cosine similarities
+# Filling values to the blank matrix with cosine similarities
 
 for i in range(0, len(itemBased.columns)) :
     for j in range(0, len(itemBased.columns)) :
       itemBased.ix[i,j] = 1-cosine(musicData.ix[:,i],musicData.ix[:,j])
 
-predictions = pd.DataFrame(index = itemBased.columns, columns=[range(1,11)])
+predictions = pd.DataFrame(index = itemBased.columns, columns=[range(1,21)])
  
 for i in range(0, len(itemBased.columns)):
-    predictions.ix[i,:10] = itemBased.ix[1:,i].sort_values(ascending = False)[:10].index
+    predictions.ix[i,:20] = itemBased.ix[1:,i].sort_values(ascending = False)[:20].index
 
-print(predictions.head(5).ix[5,2:5])
+print(predictions.head(10).ix[10,2:5])
     
